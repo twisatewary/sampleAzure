5e79c515901e2aa4374a021ac978325dad3ee8c9
twisatewary <41572895+twisatewary@users.noreply.github.com>
2018-12-11 16:10:17 +0530

    Update runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -1,4 +1,10 @@
-@echo off
-:: This program just displays Hello World
-set message=Hello World
-echo %message%
+#!/bin/bash
+echo "Hello World"
+echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
+echo "AGENT_WORKFOLDER contents:"
+ls -1 $AGENT_WORKFOLDER
+echo "AGENT_BUILDDIRECTORY is $AGENT_BUILDDIRECTORY"
+echo "AGENT_BUILDDIRECTORY contents:"
+ls -1 $AGENT_BUILDDIRECTORY
+echo "SYSTEM_HOSTTYPE is $SYSTEM_HOSTTYPE"
+echo "Over and out."
